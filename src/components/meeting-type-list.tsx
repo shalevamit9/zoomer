0198cbf082bebb176a979372337eedc28b1aea8c
Amit Shalev <amitshalev@Amits-MacBook-Pro.local>
2024-06-07 19:33:30 +0300

    add reuseable modal

--- src/components/meeting-type-list.tsx
+++ src/components/meeting-type-list.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import HomeCard from './home-card';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './meeting-modal';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -11,13 +12,15 @@ const MeetingTypeList = () => {
     'isScheduleMeeting' | 'isJoinMeeting' | 'isInstantMeeting' | undefined
   >();
 
+  function createMeeting() {}
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState('isJoinMeeting')}
+        handleClick={() => setMeetingState('isInstantMeeting')}
         className="bg-orange-1"
       />
       <HomeCard
@@ -40,6 +43,14 @@ const MeetingTypeList = () => {
         description="via invitation link"
         handleClick={() => setMeetingState('isJoinMeeting')}
         className="bg-yellow-1"
+      />
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an instant meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
